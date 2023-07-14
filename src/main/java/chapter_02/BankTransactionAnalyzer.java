a2d6067ffce40c086080ecd05afcc23311003dfd
sanikani <sanikani@naver.com>
2023-07-15 00:13:37 +0900

    결합도 개선, 테스트 구현

--- src/main/java/chapter_02/BankTransactionAnalyzer.java
+++ src/main/java/chapter_02/BankTransactionAnalyzer.java
@@ -5,21 +5,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Month;
-import java.util.ArrayList;
+
 import java.util.List;
 
 
 public class BankTransactionAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
 
-    public static void main(final String[] args) throws IOException {
-        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
-
-
-        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException{
+        final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
 
-        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
         collectSummary(bankStatementProcessor);
